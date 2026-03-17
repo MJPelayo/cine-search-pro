6dc5dd5f70b51e516e7b2b354c1734732203eb34
Marlon Pelayo <2021153565@ub.edu.bz>
2026-03-17 17:19:54 -0600

    Create SearchComponent base class

--- js/SearchComponent.js
+++ js/SearchComponent.js
@@ -2,3 +2,15 @@ import SearchComponent from "./SearchComponent.js";
 
 const app = new SearchComponent();
 app.init();
+
+export default class SearchComponent {
+
+  constructor(){
+    this.input = document.getElementById("searchBox");
+  }
+
+  init(){
+    console.log("App started");
+  }
+
+}
